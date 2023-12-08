63d241efc357bbd3864882e1975b639e871c742d
hhshhhskk <hhshhhskk@naver.com>
2023-12-09 02:37:18 +0900

    feat: 글작성페이지 틀 구현/fontawesome, draggable 라이브러리 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "../src/styles/common/App.css";
+import CreatePage from "./pages/createpage/CreatePage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <CreatePage />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/createpage/CreatePage.js
@@ -0,0 +1,63 @@
+import styled from "@emotion/styled";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import CreatePageForm from "./CreatePageForm";
+
+const Wrapper = styled.div`
+  position: relative;
+  width: 100%;
+  max-width: 600px;
+  max-height: 900px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Device = styled.div`
+  /* background-color: greenyellow; */
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 900px;
+`;
+
+const CreatePageHeader = styled.div`
+  /* background-color: red; */
+  position: relative;
+  width: 100%;
+  height: 10%;
+  display: flex;
+  justify-content: left;
+  align-items: center;
+`;
+
+const CreatePageMain = styled.div`
+  /* background-color: blue; */
+  position: relative;
+  width: 100%;
+  height: 90%;
+`;
+
+const CreatePage = () => {
+  return (
+    <Wrapper>
+      <Device>
+        <CreatePageHeader>
+          <FontAwesomeIcon
+            icon={faChevronLeft}
+            style={{ width: 20, height: 20, marginLeft: 20 }}
+          />
+        </CreatePageHeader>
+        <CreatePageMain>
+          <CreatePageForm />
+        </CreatePageMain>
+      </Device>
+    </Wrapper>
+  );
+};
+
+export default CreatePage;
