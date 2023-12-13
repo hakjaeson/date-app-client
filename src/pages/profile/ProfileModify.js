04db05b8ea080e33e6ed804c1199f8e3f43b2f8c
zdmszdms <dev.zdms@gamil.com>
2023-12-13 19:35:44 +0900

    docs : Profile 수정페이지와 파트너페이지 생성 및 구조, 디자인 작업

--- src/pages/profile/ProfileModify.js
+++ src/pages/profile/ProfileModify.js
@@ -1,7 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  ProfileModifyHeader,
+  ProfileModifyImg,
+  ProfileModifyMain,
+  ProfileModifyWrap,
+} from "../../styles/diarystyles/profilepage/profilemodifiystyle";
 
-const ProfileModify = () => {
-  return <div>ProfileModify</div>;
+const ProfileModify = props => {
+  const [defaultImg, setdefaultImg] = useState("");
+
+  return (
+    <ProfileModifyWrap>
+      <ProfileModifyHeader>
+        <Link to="/profile">
+          <img src={`${process.env.PUBLIC_URL}/images/bt_back.svg`} />
+        </Link>
+      </ProfileModifyHeader>
+
+      <ProfileModifyMain>
+        <ProfileModifyImg>
+          <img
+            className="profileimg"
+            src="https://picsum.photos/300/300"
+            alt="myimg"
+          />
+          <div>
+            <img
+              className="profileimgpartner"
+              src="https://picsum.photos/100/100"
+              alt="partnerimg"
+            />
+          </div>
+          <form>
+            <label>img url</label>
+            <input type="text"></input>
+          </form>
+        </ProfileModifyImg>
+
+        <div>
+          <span>PROFILE</span>
+          <form>
+            <hr />
+            <label>이름 : </label>
+            <input type="text"></input>
+            <hr />
+
+            <label>생년월일 : </label>
+            <input type="date"></input>
+            <hr />
+
+            <label>처음 만난 날 : </label>
+            <input type="date"></input>
+            <hr />
+
+            <div>
+              <input type="submit" value="프로필 수정 완료"></input>
+            </div>
+          </form>
+        </div>
+      </ProfileModifyMain>
+    </ProfileModifyWrap>
+  );
 };
 
 export default ProfileModify;
---
+++ src/styles/diarystyles/profilepage/profilemodifiystyle.js
@@ -0,0 +1,24 @@
+import styled from "@emotion/styled";
+
+export const ProfileModifyWrap = styled.div`
+  position: relative;
+`;
+export const ProfileModifyHeader = styled.header`
+  position: relative;
+`;
+export const ProfileModifyMain = styled.header`
+  position: relative;
+`;
+export const ProfileModifyImg = styled.header`
+  position: relative;
+  .profileimg {
+    position: relative;
+    border-radius: 70%;
+  }
+  .profileimgpartner {
+    position: absolute;
+    border-radius: 70%;
+    top: 200px;
+    left: 200px;
+  }
+`;
