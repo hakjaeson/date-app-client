26098ec13f55e2d7ad93c76fca06fad70fd3c225
dev.xxolkim <dev.xxolkim@gmail.com>
2023-12-13 18:00:49 +0900

    feat : Login 페이지 Login Error 메시지 출력 작업

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   IdForm,
   LoginButton,
   LoginContent,
+  LoginError,
   LoginFooter,
   LoginForm,
   LoginLabel,
@@ -13,6 +14,29 @@ import {
 } from "../../styles/diarystyles/login/loginstyle";
 
 const Login = () => {
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+  const [errorMg, setErrorMg] = useState("");
+  const handleChangeId = e => {
+    setId(e.target.value);
+  };
+  const handleChangePw = e => {
+    setPw(e.target.value);
+  };
+  const handleClickLogin = () => {
+    if (id === "") {
+      setErrorMg("올바른 아이디를 입력하세요.");
+      return false;
+    }
+    if (pw === "") {
+      setErrorMg("올바른 비밀번호를 입력하세요.");
+      return false;
+    }
+    if (pw.length >= 9) {
+      setErrorMg("비밀번호는 8글자 이하로 입력하세요.");
+    }
+    console.log("화면이동");
+  };
   return (
     <LoginWrapper>
       <LoginContent>
@@ -23,16 +47,37 @@ const Login = () => {
           <div>
             <IdForm>
               <LoginLabel>ID</LoginLabel>
-              <input type="text" name="id" />
+              <input
+                type="text"
+                name="id"
+                value={id}
+                onChange={e => {
+                  handleChangeId(e);
+                }}
+              />
             </IdForm>
             <PasswordForm>
               <LoginLabel>PASSWORD</LoginLabel>
-              <input type="password" name="password"></input>
+              <input
+                type="password"
+                name="password"
+                value={pw}
+                onChange={e => {
+                  handleChangePw(e);
+                }}
+              ></input>
             </PasswordForm>
           </div>
         </LoginForm>
+        <LoginError>{errorMg}</LoginError>
         <LoginFooter>
-          <LoginButton>로그인</LoginButton>
+          <LoginButton
+            onClick={() => {
+              handleClickLogin();
+            }}
+          >
+            로그인
+          </LoginButton>
           <SigninButton>회원가입</SigninButton>
         </LoginFooter>
       </LoginContent>
